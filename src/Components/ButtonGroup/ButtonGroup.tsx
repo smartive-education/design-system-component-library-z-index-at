3da01dcd47a8f63481d254b345d175347aa5a5e6
Ricardo Vögeli <ricardo.voegeli@skycell.ch>
2022-12-12 19:32:02 +0100

    Button Groups params

--- src/Components/ButtonGroup/ButtonGroup.tsx
+++ src/Components/ButtonGroup/ButtonGroup.tsx
@@ -1,36 +1,54 @@
-import React, { FC, useCallback } from "react";
+import React, { FC } from "react";
 
 import { Button } from "../Button";
 
 type ButtonGroupProps = {
-  name: string;
-  onClick: () => void;
+  LId: string,
+  LColor: string,
+  LSize: string,
+  LLabel: string,
+  LIcon: string,
+  LOnClick: () => void,
+  RId: string,
+  RColor: string,
+  RSize: string,
+  RLabel: string,
+  RIcon: string,
+  ROnClick: () => void,
 };
 
-export const ButtonGroup: FC<ButtonGroupProps> = ({ onClick }) => {
-  const handleUpload = useCallback(() => {
-    onClick();
-  }, []);
-  const handleSend = useCallback(() => {
-    onClick();
-  }, []);
+export const ButtonGroup: FC<ButtonGroupProps> = ({
+  LId,
+  LColor,
+  LSize,
+  LLabel,
+  LIcon,
+  LOnClick,
+  RId,
+  RColor,
+  RSize,
+  RLabel,
+  RIcon,
+  ROnClick,
+}) => {
 
   return (
     <div className="grid grid-cols-2 gap-2">
       <Button
-        id="upload"
-        size="M"
-        label="Bild Hochladen"
-        onClick={handleUpload}
-        icon="upload"
+        id={LId}
+        color={LColor}
+        size={LSize}
+        label={LLabel}
+        onClick={LOnClick}
+        icon={LIcon}
       />
       <Button
-        id="send"
-        size="M"
-        color="Violet"
-        label="Absenden"
-        onClick={handleSend}
-        icon="send"
+        id={RId}
+        color={RColor}
+        size={RSize}
+        label={RLabel}
+        onClick={ROnClick}
+        icon={RIcon}
       />
     </div>
   );
