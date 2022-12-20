433c4ba4aee550db1f5f2d7c183c0324526b2a25
Ricardo Vögeli <ricardo.voegeli@skycell.ch>
2022-12-20 19:27:08 +0100

    edit added

--- src/Components/ProfilePicture/ProfilePicture.tsx
+++ src/Components/ProfilePicture/ProfilePicture.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+import { Icon, IconColor } from '../Icon';
+
 export type AvatarSize = 'small' | 'medium' | 'large';
 
 interface ProfilePictureProps {
@@ -7,24 +9,47 @@ interface ProfilePictureProps {
   src: string;
   size: AvatarSize;
   onClick: () => void;
+  onEdit: () => void;
+  edit: boolean;
 }
+const defaultProps: Partial<ProfilePictureProps> = {
+  edit: false,
+};
 
-export const ProfilePicture: FC<ProfilePictureProps> = ({ name, src, size, onClick }) => {
-  return (
-    <div
-      className={`cursor-pointer 
-    rounded-full 
-    object-cover overflow-hidden
-    ${
-      size === 'small'
-        ? 'h-10 w-10'
-        : size === 'medium'
-        ? 'h-16 w-16 outline outline-8 outline-slate-100'
-        : 'h-40 w-40 outline outline-8 outline-slate-100'
+const getClasses = ((size: string) => {
+  if(size === 'small') {
+    return {
+      size: 'h-10 w-10',
     }
-    `}
-    >
+  }
+  if(size ==='medium') {
+    return {
+      size: 'h-16 w-16',
+      div: 'outline outline-8 outline-slate-100',
+    }
+  }
+  return {
+    size: 'h-40 w-40',
+    image: 'outline outline-8 outline-slate-100'
+  }
+})
+export const ProfilePicture: FC<ProfilePictureProps> = ({ name, src, size, onClick, edit, onEdit }) => {
+  const sizes = getClasses(size)
+
+  return (
+    <div className={`relative ${sizes.size}`}>
+    <div
+      className={`cursor-pointer rounded-full object-cover overflow-hidden ${sizes.div}`}>
       <img src={src} alt={name} onClick={onClick} className="hover:scale-125 duration-700 ease-in-out" />
+    </div>
+      {edit && size === 'large' &&
+       (
+        <div className='absolute bottom-0 cursor-pointer right-0 bg-slate-600 p-4 rounded-full' onClick={onEdit}>
+          <Icon id='edit' size={16} color={IconColor.White} />
+        </div>
+       )}
     </div>
   );
 };
+
+ProfilePicture.defaultProps = defaultProps;
