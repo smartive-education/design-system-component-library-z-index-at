ee21ea01b8d2d57bc15df0d487b56fe9000afbaa
Attila Toth <tthttl@gmail.com>
2022-12-23 10:51:27 +0100

    finish file input

--- src/Components/Input/FileInput/FileInput.tsx
+++ src/Components/Input/FileInput/FileInput.tsx
@@ -10,6 +10,7 @@ export interface FileInputProps {
   label: string;
   allowedFileSize: number;
   allowedExtensions: RegExp;
+  addFileFn: (file: File) => void;
 }
 
 export const FileInput: FC<FileInputProps> = ({
@@ -18,8 +19,11 @@ export const FileInput: FC<FileInputProps> = ({
   label,
   allowedFileSize = defaultFileSize,
   allowedExtensions = defaultExtensions,
+  addFileFn,
 }) => {
-  const [value, setValue] = useState('');
+  const [fileName, setFileName] = useState('');
+  const [isFileSelected, setIsFileSelected] = useState(false);
+  const [isOverDragArea, setIsOverDragArea] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const inputId = uuid();
   const inputRef = useRef<HTMLInputElement>(null);
@@ -30,13 +34,18 @@ export const FileInput: FC<FileInputProps> = ({
 
   const handleChange = (event: React.ChangeEvent): void => {
     const element = event.target as HTMLInputElement;
-    if (!isFileSizeValid(element.files?.item(0), allowedFileSize)) {
-      setErrorMessage(() => 'error.validation.sizeOverflow');
-    } else if (!isFileExtensionValid(element.value, allowedExtensions)) {
-      setErrorMessage(() => 'error.validation.invalidExtension');
-    } else {
-      setErrorMessage(() => '');
-      setValue(() => element.value);
+    const file = element.files?.item(0);
+    if (file) {
+      if (!isFileSizeValid(file, allowedFileSize)) {
+        setErrorMessage(() => 'error.validation.sizeOverflow');
+      } else if (!isFileExtensionValid(element.value, allowedExtensions)) {
+        setErrorMessage(() => 'error.validation.invalidExtension');
+      } else {
+        setErrorMessage(() => '');
+        setIsFileSelected(() => true);
+        setFileName(() => element.value);
+        addFileFn(file);
+      }
     }
   };
 
@@ -48,8 +57,8 @@ export const FileInput: FC<FileInputProps> = ({
   const handleDrop = (event: React.DragEvent): void => {
     preventBrowserDefaults(event);
     const files = event.dataTransfer.files;
-
-    if(files.length > 1){
+    setIsOverDragArea(() => false);
+    if (files.length > 1) {
       setErrorMessage(() => 'error.validation.tooManyFiles');
     } else if (!isFileSizeValid(files[0], allowedFileSize)) {
       setErrorMessage(() => 'error.validation.sizeOverflow');
@@ -57,32 +66,55 @@ export const FileInput: FC<FileInputProps> = ({
       setErrorMessage(() => 'error.validation.invalidExtension');
     } else {
       setErrorMessage(() => '');
-      setValue(() => inputRef.current?.value ?? '');
+      setIsFileSelected(() => true);
+      setFileName(() => files[0].name);
+      addFileFn(files[0]);
     }
+  };
 
+  const handleDragEnter = (event: React.DragEvent): void => {
+    preventBrowserDefaults(event);
+    setIsOverDragArea(() => true);
+  };
+
+  const handleDragLeave = (event: React.DragEvent): void => {
+    preventBrowserDefaults(event);
+    setIsOverDragArea(() => false);
   };
 
   return (
-    <div className={`flex flex-col`}>
+    <div className='flex flex-col'>
       <div
         onDrop={handleDrop}
-        onDragOver={preventBrowserDefaults}
-        onDragEnter={preventBrowserDefaults}
-        onDragLeave={preventBrowserDefaults}
-        className="flex flex-col items-center border-2 border-dotted rounded-lg mb-4 p-6 bg-slate-100"
+        onDragOver={handleDragEnter}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        className={`flex flex-col border-2 border-dotted rounded-lg mb-4 p-6 ${
+          isOverDragArea ? 'bg-slate-300' : 'bg-slate-100'
+        }`}
       >
-        <div>
-          <Icon id="upload" color={IconColor.Gray} size={30} />
-        </div>
-        <h4 className="text-2xl text-slate-500 font-semibold">{title}</h4>
-        <p className="text-slate-400">{restrictions}</p>
+        {!isFileSelected ? (
+          <div className="flex flex-col items-center h-20">
+            <div>
+              <Icon id="upload" color={IconColor.Gray} size={30} />
+            </div>
+            <h4 className="text-2xl text-slate-500 font-semibold">{title}</h4>
+            <p className="text-slate-400">{restrictions}</p>
+          </div>
+        ) : (
+          <div className="h-20 flex flex-col justify-center items-center">
+            <div className="rounded-lg bg-slate-300 pt-4 px-2 pb-2 mb-2">
+              <Icon id="check" color={IconColor.Gray} size={30} />
+            </div>
+            <p className="text-slate-600 text-center">{`${fileName} wurde hinzugefügt`}</p>
+          </div>
+        )}
       </div>
       <div>
         <input
           ref={inputRef}
           id={inputId}
           type="file"
-          value={value}
           onChange={handleChange}
           accept="image/png, image/jpeg, image/jpg"
           multiple={false}
