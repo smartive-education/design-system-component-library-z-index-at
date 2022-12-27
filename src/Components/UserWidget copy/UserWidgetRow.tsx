f8f8b48758e7add9c8dc3d7d426bd4ba23cf187d
Ricardo Vögeli <ricardo.voegeli@skycell.ch>
2022-12-27 20:02:00 +0100

    fixe pixels removed

--- src/Components/UserWidget copy/UserWidgetRow.tsx
+++ src/Components/UserWidget copy/UserWidgetRow.tsx
@@ -7,13 +7,13 @@ export type AvatarSize = 'small' | 'medium' | 'large';
 export const UserWidgetRow = () => {
   return (
     <div className="flex flex-row items-center justify-around">
-      <div className="p-2 min-w-[216px] w-[216px]">
+      <div className="p-2 w-56">
         <UserWidget src="/assets/images/profile/r.vogt.jpg" name="Damian Caduff" size="small" username="damiancaduff" />
       </div>
-      <div className="p-2 min-w-[216px] w-[216px]">
+      <div className="p-2 w-56">
         <UserWidget src="/assets/images/profile/r.vogt.jpg" name="Robert Vogt" size="small" username="robertvogt" />
       </div>
-      <div className="p-2 min-w-[216px] w-[216px]">
+      <div className="p-2 w-56">
         <UserWidget
           src="/assets/images/profile/r.vogt.jpg"
           name="Chrisoph Bühler"
