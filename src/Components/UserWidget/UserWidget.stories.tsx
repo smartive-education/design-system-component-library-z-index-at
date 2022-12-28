0c4a75de7ce9b09c9728b4d3be0cfe9e7c7d462c
Ricardo Vögeli <ricardo.voegeli@skycell.ch>
2022-12-28 14:45:16 +0100

    row component deleted and row implemented in storybook

--- src/Components/UserWidget/UserWidget.stories.tsx
+++ src/Components/UserWidget/UserWidget.stories.tsx
@@ -9,6 +9,24 @@ export default {
 
 const Template: ComponentStory<typeof UserWidget> = (args) => <UserWidget {...args} />;
 
+const TemplateRow: ComponentStory<typeof UserWidget> = (args) =>
+<div className="flex flex-row items-center justify-around">
+  <div className="p-2 w-56">
+    <UserWidget src="assets/images/profile/r.vogt.jpg" name="Damian Caduff" size="small" username="damiancaduff" />
+  </div>
+  <div className="p-2 w-56">
+    <UserWidget src="assets/images/profile/r.vogt.jpg" name="Robert Vogt" size="small" username="robertvogt" />
+  </div>
+  <div className="p-2 w-56">
+    <UserWidget
+      src="assets/images/profile/r.vogt.jpg"
+      name="Chrisoph Bühler"
+      size="small"
+      username="christophbuehler"
+    />
+  </div>
+</div>
+
 export const Widget = Template.bind({});
 Widget.args = {
   src: '/assets/images/profile/r.vogt.jpg',
@@ -16,3 +34,5 @@ Widget.args = {
   size: 'small',
   username: 'robertvogt',
 };
+
+export const Row = TemplateRow.bind({});
