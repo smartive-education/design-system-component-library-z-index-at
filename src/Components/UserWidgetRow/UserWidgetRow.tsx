f78f22dc14ed4ad17c77142e733e0c4480c24070
Ricardo Vögeli <ricardo.voegeli@skycell.ch>
2022-12-28 11:40:21 +0100

    file added

---
+++ src/Components/UserWidgetRow/UserWidgetRow.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+
+import { UserWidget } from '../UserWidget';
+
+export type AvatarSize = 'small' | 'medium' | 'large';
+
+export const UserWidgetRow = () => {
+  return (
+    <div className="flex flex-row items-center justify-around">
+      <div className="p-2 w-56">
+        <UserWidget src="assets/images/profile/r.vogt.jpg" name="Damian Caduff" size="small" username="damiancaduff" />
+      </div>
+      <div className="p-2 w-56">
+        <UserWidget src="assets/images/profile/r.vogt.jpg" name="Robert Vogt" size="small" username="robertvogt" />
+      </div>
+      <div className="p-2 w-56">
+        <UserWidget
+          src="assets/images/profile/r.vogt.jpg"
+          name="Chrisoph Bühler"
+          size="small"
+          username="christophbuehler"
+        />
+      </div>
+    </div>
+  );
+};
