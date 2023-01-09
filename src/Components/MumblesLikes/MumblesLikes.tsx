878b731bf8b5ce8524812e2cbe25a3ec2dcdb636
Ricardo Vögeli <ricardo.voegeli@skycell.ch>
2023-01-09 16:24:20 +0100

    mouse over and out animation added

--- src/Components/MumblesLikes/MumblesLikes.tsx
+++ src/Components/MumblesLikes/MumblesLikes.tsx
@@ -12,6 +12,7 @@ const defaultProps: Partial<MumblesLikesProps> = {
 
 export const MumblesLikes: FC<MumblesLikesProps> = ({ state, onClick }) => {
   const [display, setDisplay] = useState(state);
+  const [expandHover, setExpandHover] = useState('');
   const handleChange = useCallback(
     (state) => {
       setDisplay(state);
@@ -20,19 +21,26 @@ export const MumblesLikes: FC<MumblesLikesProps> = ({ state, onClick }) => {
     [display]
   );
 
+  console.log(expandHover);
   return (
-    <div className="bg-slate-200 border-transparent rounded-xl p-1 inline-flex">
+    <div className="bg-slate-200 border-transparent rounded-xl p-1 flex justify-between max-w-xs">
       <div
-        className={`${
-          display === 'mumbles' ? 'bg-white text-violet-600' : ''
-        } rounded-xl p-3 cursor-pointer text-xl mr-5`}
+        className={`${display === 'mumbles' ? 'bg-white text-violet-600' : ''} rounded-xl p-2 cursor-pointer text-xl ${
+          expandHover === 'mumbles' ? 'pr-5 duration-150' : 'pr-2 duration-150'
+        }`}
         onClick={() => handleChange('mumbles')}
+        onMouseOver={() => setExpandHover('likes')}
+        onMouseOut={() => setExpandHover('')}
       >
         Deine Mumbles
       </div>
       <div
-        className={`${display === 'likes' ? 'bg-white text-violet-600' : ''} rounded-xl p-3 cursor-pointer text-xl`}
+        className={`${display === 'likes' ? 'bg-white text-violet-600' : ''} rounded-xl p-2 cursor-pointer text-xl ${
+          expandHover === 'likes' ? 'pl-5 duration-150' : 'pl-2 duration-150'
+        }`}
         onClick={() => handleChange('likes')}
+        onMouseOver={() => setExpandHover('mumbles')}
+        onMouseOut={() => setExpandHover('')}
       >
         Deine Likes
       </div>
