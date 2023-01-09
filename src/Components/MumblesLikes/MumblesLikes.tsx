2ef500060db9632b718283f633c6eb533f7d6cd8
Ricardo Vögeli <ricardo.voegeli@skycell.ch>
2023-01-09 21:04:05 +0100

    replaced with scale

--- src/Components/MumblesLikes/MumblesLikes.tsx
+++ src/Components/MumblesLikes/MumblesLikes.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useCallback, useEffect } from 'react';
+import React, { FC, useState, useCallback, useEffect, useMemo } from 'react';
 
 export type State = 'mumbles' | 'likes';
 
@@ -12,7 +12,7 @@ const defaultProps: Partial<MumblesLikesProps> = {
 
 export const MumblesLikes: FC<MumblesLikesProps> = ({ state, onClick }) => {
   const [display, setDisplay] = useState(state);
-  const [expandHover, setExpandHover] = useState('');
+  const [expandHover, setExpandHover] = useState(false);
   const handleChange = useCallback(
     (state) => {
       setDisplay(state);
@@ -28,28 +28,40 @@ export const MumblesLikes: FC<MumblesLikesProps> = ({ state, onClick }) => {
     setDisplay(state);
   }, [state]);
 
+  const expandHoverClasses = useMemo(() => {
+    if (expandHover) {
+      return display === 'mumbles' ? 'origin-left scale-x-105 duration-150' : 'origin-right scale-x-105';
+    }
+    return display === 'mumbles' ? 'w-[10rem] duration-150' : 'w-[8rem] duration-150';
+  }, [display, expandHover]);
+
   return (
-    <div className="bg-slate-200 border-transparent rounded-xl p-1 flex justify-between max-w-xs">
+    <div className="bg-slate-200 border-transparent rounded-xl p-1 flex justify-between max-w-xs relative z-0">
       <div
-        className={`${display === 'mumbles' ? 'bg-white text-violet-600' : ''} rounded-xl p-2 cursor-pointer text-xl ${
-          expandHover === 'mumbles' ? 'pr-5 duration-150' : 'pr-2 duration-150'
-        }`}
+        className={`${display === 'mumbles' ? 'text-violet-600' : ''} p-2 cursor-pointer text-xl z-20`}
         onClick={() => handleChange('mumbles')}
-        onMouseOver={() => setExpandHover('likes')}
-        onMouseOut={() => setExpandHover('')}
+        onMouseOver={() => setExpandHover(display !== 'mumbles' && true)}
+        onMouseOut={() => setExpandHover(false)}
       >
         Deine Mumbles
       </div>
       <div
-        className={`${display === 'likes' ? 'bg-white text-violet-600' : ''} rounded-xl p-2 cursor-pointer text-xl ${
-          expandHover === 'likes' ? 'pl-5 duration-150' : 'pl-2 duration-150'
-        }`}
+        className={`${display === 'likes' ? 'text-violet-600' : ''} p-2 cursor-pointer text-xl z-20`}
         onClick={() => handleChange('likes')}
-        onMouseOver={() => setExpandHover('mumbles')}
-        onMouseOut={() => setExpandHover('')}
+        onMouseOver={() => setExpandHover(display !== 'likes' && true)}
+        onMouseOut={() => setExpandHover(false)}
       >
         Deine Likes
       </div>
+      <div
+        className={`absolute top-0 bottom-0 rounded-xl m-1 bg-white z-10
+          ${
+            display === 'mumbles'
+              ? 'left-0 duration-150 translate-x-0 w-[10.8rem]'
+              : 'duration-150 translate-x-[11.4rem] w-[7.8rem]'
+          }
+          ${expandHoverClasses}`}
+      ></div>
     </div>
   );
 };
