2fe3da3a688e60f09ca3922d1af3a25b8b531c8f
Attila Toth <tthttl@gmail.com>
2023-04-18 22:37:54 +0200

    Revert "fix: remove fix height from profile"
    This reverts commit fd6b0be8f05e6e13008c0a257f22001c6b616089.

--- src/Components/ProfilePicture/ProfilePicture.tsx
+++ src/Components/ProfilePicture/ProfilePicture.tsx
@@ -6,33 +6,28 @@ export const ProfilePicture: FC<ProfilePictureProps> = ({ name, src = '', size, 
   const getClasses = useCallback((size: string) => {
     if (size === 'small') {
       return {
-        size: 'h-auto w-10',
-        div: '',
-        height: 'h-10',
+        size: 'h-10 w-10',
+        div: 'h-10',
       };
     }
     if (size === 'medium') {
       return {
-        size: 'h-auto w-16',
-        div: 'outline outline-8 outline-slate-100',
-        height: 'h-16',
+        size: 'h-16 w-16',
+        div: 'outline outline-8 outline-slate-100 h-16',
       };
     }
     return {
-      size: 'h-auto w-40',
-      div: 'outline outline-8 outline-slate-100',
-      height: 'h-40',
+      size: 'h-40 w-40',
+      div: 'outline outline-8 outline-slate-100 h-40',
     };
   }, []);
 
   return (
     <div className={`relative ${getClasses(size).size}`}>
-      <div
-        className={`cursor-pointer rounded-full object-cover overflow-hidden bg-violet-100 ${getClasses(size).div} ${
-          !src ? getClasses(size).height : ''
-        }`}
-      >
-        {src && <img src={src} alt={name} onClick={onClick} className="hover:scale-125 duration-700 ease-in-out" />}
+      <div className={`cursor-pointer rounded-full object-cover overflow-hidden bg-violet-100 ${getClasses(size).div}`}>
+        {src && (
+          <img src={src} alt={name} onClick={onClick} className="hover:scale-125 duration-700 ease-in-out h-full" />
+        )}
       </div>
       {edit && size === 'large' && (
         <div className="absolute bottom-0 cursor-pointer right-0 bg-slate-600 p-4 rounded-full" onClick={onEdit}>
